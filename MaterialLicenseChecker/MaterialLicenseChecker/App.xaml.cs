12af202867a254bb2cb982306d9487552fca04f9
terapotan <gameblog60@gmail.com>
2020-07-19 12:46:45 +0900

    MVVMパターンが適用出来るように、コードを修正

--- MaterialLicenseChecker/MaterialLicenseChecker/App.xaml.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MaterialLicenseChecker.Views;
 
 namespace MaterialLicenseChecker
 {
@@ -13,5 +14,12 @@ namespace MaterialLicenseChecker
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var w = new MainView();
+            w.Show();
+        }
     }
 }
