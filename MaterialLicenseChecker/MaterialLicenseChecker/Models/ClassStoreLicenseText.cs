e2f669203329ccc120c8ba0ce6aa352d7ac146b5
terapotan <gameblog60@gmail.com>
2020-07-23 16:02:43 +0900

    ライセンステキストの文章を追加

--- MaterialLicenseChecker/MaterialLicenseChecker/Models/ClassStoreLicenseText.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/Models/ClassStoreLicenseText.cs
@@ -8,5 +8,21 @@ namespace MaterialLicenseChecker.Models
 {
     class ClassStoreLicenseText
     {
+        /// <summary>
+        /// ライセンステキストを辞書型で保管する。
+        /// 1番目:利用規約のサイト名(キーとなる)
+        /// 2番目:利用規約の内容(テキスト)
+        /// </summary>
+        private Dictionary<string, string> _licenseTextDictionary;
+        public ClassStoreLicenseText()
+        {
+            _licenseTextDictionary = new Dictionary<string,string>();
+            _licenseTextDictionary.Add("A", "サイトAの利用規約");
+            _licenseTextDictionary.Add("B", "サイトBの利用規約");
+            _licenseTextDictionary.Add("C", "サイトCの利用規約");
+            _licenseTextDictionary.Add("D", "サイトDの利用規約");
+            _licenseTextDictionary.Add("E", "サイトEの利用規約");
+
+        }
     }
 }
