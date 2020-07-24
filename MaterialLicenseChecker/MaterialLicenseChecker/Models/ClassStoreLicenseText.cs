90aed7455623f5325244bab11acc3851a689aa34
terapotan <gameblog60@gmail.com>
2020-07-24 22:53:24 +0900

    XMLファイルの読み込み機能を追加

--- MaterialLicenseChecker/MaterialLicenseChecker/Models/ClassStoreLicenseText.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/Models/ClassStoreLicenseText.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Reflection;
 
 namespace MaterialLicenseChecker.Models
 {
@@ -18,15 +20,26 @@ namespace MaterialLicenseChecker.Models
         //将来的には、2番目のstringは単なる文字列ではなく、ユーザー定義のクラスとなるだろう。
         //まぁ先のことだ。
 
+        //TODO:現状このクラスで、ファイルの読み書きを行っている。
+        //別のクラスへ分離したほうがいい気がするが、それはXDocument等で取得したインスタンス
+        //を別の形式に変換することになり、あまり意味がないような気がする。
+        //ということで、FIXMEにしていないが何か不都合があれば、分離を実行するように。
+
+
         public ClassStoreLicenseText()
         {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            string runingFilePath = assembly.Location;
+            System.IO.FileInfo fi = new System.IO.FileInfo(runingFilePath);
+            string runingDirectoryPath = fi.Directory.FullName;
+
+            XDocument xml = XDocument.Load(runingDirectoryPath + "/ExportResources/LicenseTexts.xml");  ;
             _licenseTextDictionary = new Dictionary<string,string>();
             _licenseTextDictionary.Add("A", "サイトAの利用規約");
             _licenseTextDictionary.Add("B", "サイトBの利用規約");
             _licenseTextDictionary.Add("C", "サイトCの利用規約");
             _licenseTextDictionary.Add("D", "サイトDの利用規約");
             _licenseTextDictionary.Add("E", "サイトEの利用規約");
-
         }
 
         /// <summary>
