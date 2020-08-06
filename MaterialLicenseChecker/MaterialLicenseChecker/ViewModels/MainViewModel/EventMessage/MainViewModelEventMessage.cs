4d9bfccddd00c5d629b74c02a3ea70f6dcad6adc
terapotan <gameblog60@gmail.com>
2020-08-06 17:21:41 +0900

    MainViewModelEventMessage系のクラスを実装

---
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/MainViewModel/EventMessage/MainViewModelEventMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialLicenseChecker;
+
+namespace MaterialLicenseChecker.ViewModels
+{
+    class MainViewModelEventMessage : BaseEventMessage
+    {
+        public MainViewModelEventMessage(object sender) : base(sender)
+        {
+
+        }
+    }
+}
---
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/MainViewModel/EventMessage/MainViewModelEventMessenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialLicenseChecker.ViewModels
+{
+    class MainViewModelEventMessenger
+    {
+        public static MainViewModelEventMessenger Default { get; } = new MainViewModelEventMessenger();
+
+    }
+}
