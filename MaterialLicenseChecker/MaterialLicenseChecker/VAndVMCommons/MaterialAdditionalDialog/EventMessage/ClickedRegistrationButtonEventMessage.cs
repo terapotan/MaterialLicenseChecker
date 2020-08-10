56834c6c8e662eccf088f54bcdd7b8344a489c06
terapotan <gameblog60@gmail.com>
2020-08-10 12:41:31 +0900

    ClickedRegisterationクラス追加

---
+++ MaterialLicenseChecker/MaterialLicenseChecker/VAndVMCommons/MaterialAdditionalDialog/EventMessage/ClickedRegistrationButtonEventMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialLicenseChecker.VAndVMCommons.MaterialAdditionalDialog
+{
+    class ClickedRegistrationButtonEventMessage : BaseEventMessage
+    {
+        public ClickedRegistrationButtonEventMessage(object sender) : base(sender)
+        {
+
+        }
+
+        public string MaterialName = "";
+        public string MaterialSiteName = "";
+        public string MaterialFilePath = "";
+
+    }
+}
