221b28ba73e9363889077819bbb35a8e9d517508
terapotan <gameblog60@gmail.com>
2020-10-04 17:09:40 +0900

    インタフェースを定義

---
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/MaterialAdditionalDialog/AddMaterialDataToFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialLicenseChecker.ViewModels.MaterialAdditionalDialog
+{
+    /// <summary>
+    /// 素材名を管理するファイルに素材データを追加する
+    /// </summary>
+    public class AddMaterialDataToFile
+    {
+    }
+}
