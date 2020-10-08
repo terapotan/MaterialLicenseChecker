09f488df0377cb450cb4c565a8bfd694efab015d
terapotan <gameblog60@gmail.com>
2020-10-08 21:29:04 +0900

    VAndVMCommonsにあった関連ファイルを削除

--- MaterialLicenseChecker/MaterialLicenseChecker/Views/MaterialSiteAdditionalDialog.xaml.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/Views/MaterialSiteAdditionalDialog.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using MaterialLicenseChecker.VAndVMCommons.MaterialSiteAdditionalDialog;
 using MaterialSite = MaterialLicenseChecker.ViewModels.MaterialSiteAdditionalDialog;
 
 
