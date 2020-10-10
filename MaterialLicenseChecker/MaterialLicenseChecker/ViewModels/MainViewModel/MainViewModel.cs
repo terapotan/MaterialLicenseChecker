0543187be11f87dfa22540c67cd9a1458a9ce283
terapotan <gameblog60@gmail.com>
2020-10-10 16:28:08 +0900

    MakeDisplayedLicenseText関数を削除

--- MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/MainViewModel/MainViewModel.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/MainViewModel/MainViewModel.cs
@@ -33,48 +33,5 @@ namespace MaterialLicenseChecker.ViewModels.MainViewModel
             FileInstance.DeleteMaterialData(cmd.ListFromDeletedMaterialName);
         }
 
-        /*
-
-        //以下のコードは「getはpublicにしたいが、setはprivateとしたい」目的で書かれている。
-        private DelegateCommand _ShowDialogCommand;
-
-
-
-        public DelegateCommand ShowDialogCommand
-        {
-            get
-            {
-                //Privateな変数に代入することをを忘れないように!!!
-                //Publicな変数に対して動作を行うと……何も表示されなくなる
-                if(_ShowDialogCommand == null)
-                {
-                    _ShowDialogCommand = new DelegateCommand(
-                        _ => MessageBox.Show(
-                            MakeDisplayedLicenseText()));
-                }
-
-                return _ShowDialogCommand;
-            }
-        }
-
-        private DelegateCommand _ShowMaterialSiteAdditionalDialog;
-
-        public DelegateCommand ShowMaterialSiteAdditionalDialog
-        {
-            get
-            {
-                if (_ShowMaterialSiteAdditionalDialog == null)
-                {
-                    var msg = new MainVewModelMessage(this);
-              
-                    _ShowMaterialSiteAdditionalDialog = new DelegateCommand(
-                        _ => MainViewModelMessanger.Default.ExecuteAction<MainVewModelMessage>(this,msg));
-                }
-
-                return _ShowMaterialSiteAdditionalDialog;
-            }
-        }
-        */
-
     }
 }
