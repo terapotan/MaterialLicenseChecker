23c6eea9609c07ae0b82efc8a506ff3a765e80b9
terapotan <gameblog60@gmail.com>
2021-01-02 14:02:16 +0900

    メインビュー有効値管理クラスの名称変更

--- MaterialLicenseChecker/MaterialLicenseChecker/Views/CMainView/MainViewItemsAvailableValueManager.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/Views/CMainView/MainViewItemsAvailableValueManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MaterialLicenseChecker.Views.CMainView
 {
-    public class ItemAvailableValueManager
+    public class MainViewItemsAvailableValueManager
     {
     }
 }
