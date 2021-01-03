369fc7ec4c62e3c0108de38a76c51d4c3301ba03
terapotan <gameblog60@gmail.com>
2021-01-03 15:20:05 +0900

    プロジェクトファイル生成関連のイベントを仮実装

---
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/GenerateProjectDialog/GenerateProjectDialogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialLicenseChecker.ViewModels.GenerateProjectDialog
+{
+    public class GenerateProjectDialogViewModel : IReceiverCommandFromView
+    {
+        public void CommandViewModelTo(GenerateProjectFile cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
