4d808757d600cf89eccf947c8216f383e54b7718
terapotan <gameblog60@gmail.com>
2021-04-02 17:29:05 +0900

    登録済みの素材配布サイトを削除したときの動作を実装

--- MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/EditingMaterialSiteSpace/DeleteMaterialSite.cs
+++ MaterialLicenseChecker/MaterialLicenseChecker/ViewModels/EditingMaterialSiteSpace/DeleteMaterialSite.cs
@@ -9,5 +9,6 @@ namespace MaterialLicenseChecker.ViewModels.EditingMaterialSiteSpace
     public class DeleteMaterialSite
     {
         public string DeletingMaterialSiteName = "";
+        public int ErrorNum = 0;//正常に終了したなら0。既にサイトが登録されている場合は-1。
     }
 }
